0eefa52eb28368c34027ad0de1e2ddacf0292516
Valdis Saules <valdis.s.coding@gmail.com>
2020-05-26 11:05:41 +0300

    added some javasript arithmethic doodles

--- js/hello_world.js
+++ js/hello_world.js
@@ -1,3 +1,41 @@
 // alert("Hello world!");
-console.log("Hello World!");
-console.log(5 + 10);
+// console.log("Hello World!");
+// console.log(5 + 10);
+var a = 9000;
+let myName = "Valdis";
+console.log("hi there");
+console.log("hi there");
+console.log("hi there");
+console.log("hi there");
+
+console.log("Hi " + myName + " your power level is over " + a);
+
+a = a + 10;
+console.log(a);
+
+var sum = 5 * 10; //bad name we can do this, but why call it sum then? no point
+console.log(sum);
+
+var result = 100 + 5 * 10; //better do this then
+console.log(result);
+result++; // same thing as result = result + 1;
+console.log(result);
+result++;
+result++;
+result++;
+result++;
+result++;
+result++; // could have just done result = result + 6
+console.log(result);
+result = result + 6;
+console.log(result);
+result--;
+result--;
+result--;
+result--;
+result--;
+result--;
+result--;
+result--;
+result--;
+console.log(result);
