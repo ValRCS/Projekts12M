7b52bed628d21ba61d0a7317de2e33b382a654dc
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-02 11:08:31 +0300

    Added dom selector examples

---
+++ js/domapi.js
@@ -0,0 +1,13 @@
+function main() {
+    console.log("Running domapi examples");
+    console.log("Running Main");
+
+    const app = document.getElementById("myApp");
+    app.style.height = "200px";
+    app.style.backgroundColor = "#AAAA11";
+    app.innerText = "My big bad application";
+
+    const boxes = document.getElementsByClassName("boxy");
+}
+
+main();
