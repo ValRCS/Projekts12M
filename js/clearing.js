e92ab144d4d9b573c5bd4f51408d8dc4f310e804
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-03 11:21:11 +0300

    stub for adding clearing elements

---
+++ js/clearing.js
@@ -0,0 +1,19 @@
+console.log("Started Clearing js");
+
+//Add Listeners to each button
+
+function addElements() {
+    console.log("Adding Elements");
+}
+
+function deleteElements() {
+    console.log("Clearing Elements");
+}
+
+function addEventHandlers() {
+    console.log("adding Handlers");
+    //add event handlers here
+    //you will need to find the elements
+}
+
+addEventHandlers();
