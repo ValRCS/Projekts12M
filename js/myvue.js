91f56bd84f9d20469b3dc1cbc6bb7ae6ffdddcc7
Valdis Saules <valdis.s.coding@gmail.com>
2020-06-05 11:53:25 +0300

    Vue methods and computer

--- js/myvue.js
+++ js/myvue.js
@@ -3,7 +3,23 @@ console.log("My view is running!");
 let app = new Vue({
     el: "#app", //where our app will attach
     data: {
-        message: "Hello Vue!",
+        message: "You loaded this page on " + new Date().toLocaleString(),
         greeting: "Sunny Day today",
+        seen: true,
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split("").reverse().join("");
+        },
+        toggleElement: function () {
+            this.seen = !this.seen; //a way of reversing Boolean
+        },
+    },
+    computed: {
+        // a computed getter
+        capitalMessage: function () {
+            // `this` points to the vm instance
+            return this.message.toUpperCase();
+        },
     },
 });
